3e5db72ebeb156a7f353e5b444f5bd05af0d0b37
Abhilash P K <abhilashpk63@gmail.com>
2026-03-16 10:33:34 +0530

    Update deploy-vm.sh

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+RESOURCE_GROUP="demo-resource-group"
+LOCATION="eastus"
+VM_NAME="demoVM"
+
+echo "Creating Resource Group..."
+
+az group create \
+--name $RESOURCE_GROUP \
+--location $LOCATION
+
+echo "Creating Virtual Machine..."
+
+az vm create \
+--resource-group $RESOURCE_GROUP \
+--name $VM_NAME \
+--image UbuntuLTS \
+--admin-username azureuser \
+--generate-ssh-keys
+
+echo "Opening port 22 for SSH..."
+
+az vm open-port \
+--port 22 \
+--resource-group $RESOURCE_GROUP \
+--name $VM_NAME
+
+echo "Deployment Completed!"
